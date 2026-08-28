c8a1caa43041c4deb4d785a1620525c0059ef504
Rolando Villatoro <rvillatoro@emt.dev>
2026-08-27 19:22:44 -0600

    feat: add environment configuration for Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA3vqQO0GV-I3zUShUoau92GcuQGQFavro",
+        authDomain: "novaltsdashboard.firebaseapp.com",
+        projectId: "novaltsdashboard",
+        storageBucket: "novaltsdashboard.firebasestorage.app",
+        messagingSenderId: "468795477212",
+        appId: "1:468795477212:web:99fe150923c63eda4ff819",
+        measurementId: "G-WXZQ568KCX"
+    }
+};
